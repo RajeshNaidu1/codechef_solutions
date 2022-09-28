0838cf84aac1f7ecac37fa52317ca4f37639e60b
RajeshNaidu1 <20bcs079@iiitdwd.ac.in>
2022-09-29 01:28:48 +0530

    day-1

---
+++ the_chepeast_cab.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes hereint t;
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    if(a>b){
+	        cout<<"SECOND"<<endl;
+	    }
+	    else if(a==b){
+	        cout<<"ANY"<<endl;
+	    }
+	    else{
+	        cout<<"FIRST"<<endl;
+	    }
+	}
+	return 0;
+}
