bb534bbbecab5dccd4bcce4ebfcd24e1246afff3
RajeshNaidu1 <20bcs079@iiitdwd.ac.in>
2022-10-05 21:13:39 +0530

    strings(1)

---
+++ Strings(0-1000)/wordle.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    string s,t,m;
+	    cin>>s>>t;
+	    for(int i=0;i<5;i++){
+	        if(s[i]==t[i]){
+	            m[i]='G';
+	        }
+	        else{
+	            m[i]='B';
+	        }
+	    }
+	    for(int i=0;i<5;i++){
+	        cout<<m[i];
+	    }
+	    cout<<"\n";
+	}
+	return 0;
+}
